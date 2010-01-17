f71c29663e33da20d4576ec3acb1d0d2064a7201
Arne Blankerts <theseer@rikka.(none)>
2010-01-17 14:20:15 +0100

    load new structs

--- tests/init.php
+++ tests/init.php
@@ -1,8 +1,6 @@
 <?php
 
   require __DIR__.'/../src/directoryscanner.php';
-  require __DIR__.'/../src/filter.php';
-
-  //require 'PHPUnit/Framework.php';
-
+  require __DIR__.'/../src/filesonlyfilter.php';
+  require __DIR__.'/../src/includeexcludefilter.php';
 
