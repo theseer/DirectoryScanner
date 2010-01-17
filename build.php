896389c3ed9f55cfe563a9b12a10a79d15b0fc00
Arne Blankerts <Arne@Blankerts.de>
2010-01-17 22:21:20 +0100

    cleanup, no /.. on path

--- build.php
+++ build.php
@@ -8,18 +8,18 @@
   require 'src/includeexcludefilter.php';
   require 'src/filesonlyfilter.php';
 
-  $scanner = new \TheSeer\Tools\DirectoryScanner;
+  $scanner = new TheSeer\Tools\DirectoryScanner;
   $scanner->addInclude('*.php');
 
-  $finder = new \TheSeer\Tools\ClassFinder;
+  $finder = new TheSeer\Tools\ClassFinder;
 
   $found = $finder->parseMulti($scanner('src'));
 
-  $ab = new \TheSeer\Tools\AutoloadBuilder($found);
-  $ab->setBaseDir(realpath(__DIR__.'/..'));
+  $ab = new TheSeer\Tools\AutoloadBuilder($found);
+  $ab->setBaseDir(realpath(__DIR__));
 
   $ab->save('autoload.php');
-  
+
   echo "Done.\n";
 
 ?>
