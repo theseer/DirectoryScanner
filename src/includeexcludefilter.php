b8788282549d26eaabfec1da7f814d868addb07b
Arne Blankerts <theseer@rikka.(none)>
2010-01-17 14:21:26 +0100

    renamed package

--- src/includeexcludefilter.php
+++ src/includeexcludefilter.php
@@ -29,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @package    Scanner
+ * @package    DirectoryScanner
  * @author     Arne Blankerts <arne@blankerts.de>
  * @copyright  Arne Blankerts <arne@blankerts.de>, All rights reserved.
  * @license    BSD License
