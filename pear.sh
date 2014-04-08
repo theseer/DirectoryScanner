1442bac2567b90fd9e729c1c5662fabb070921f6
Arne Blankerts <Arne@Blankerts.de>
2014-04-09 00:06:06 +0200

    Added LICENSE to build

--- pear.sh
+++ pear.sh
@@ -3,5 +3,6 @@ rm -f Directory-Scanner*.tgz
 mkdir -p TheSeer/DirectoryScanner
 cp -r src/* TheSeer/DirectoryScanner
 cp dist.php TheSeer/DirectoryScanner/autoload.php
+cp LICENSE phpunit.xml.dist TheSeer/DirectoryScanner
 pear package
 rm -rf TheSeer
